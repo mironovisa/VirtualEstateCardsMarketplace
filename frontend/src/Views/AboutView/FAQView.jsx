c00e8b8c7789adb1fb1b1993177a1762b70f70f7
Yonatan Abramovich <yonatanabra99@gmail.com>
2023-08-19 22:55:38 +0300

    primary styling and components structure done

--- frontend/src/Views/AboutView/FAQView.jsx
+++ frontend/src/Views/AboutView/FAQView.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FAQView = () => {
     return (
-        <div className="full-screen-view">
+        <div className="full-screen-view faq">
             <h2>Frequently Asked Questions</h2>
             <p>This is a generic FAQ text.</p>
         </div>
