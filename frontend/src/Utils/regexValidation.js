c3835375c3a1ea0604202d41a1345c1700d53d14
Gidon Tuch <gidon.t@datarails.com>
2023-09-01 11:00:17 +0300

    Regex comment out

--- frontend/src/Utils/regexValidation.js
+++ frontend/src/Utils/regexValidation.js
@@ -7,12 +7,12 @@ const checkIfMatchingPattern = (fieldName, fieldValue, regexPatterns) => {
     const isValid = Object.keys(data).every(
         (fieldName) => checkIfMatchingPattern(fieldName, data[fieldName], regexPatterns)
       );
-    Object.keys(data).forEach((key) => {
-      const isValid = checkIfMatchingPattern(key, data[key]);
-      if (!isValid) {
-        console.log("Incorrect " + key + " field");
-      }
-    });
+    // Object.keys(data).forEach((key) => {
+    //   const isValid = checkIfMatchingPattern(key, data[key]);
+    //   if (!isValid) {
+    //     console.log("Incorrect " + key + " field");
+    //   }
+    // });
     return isValid;
   };
 
