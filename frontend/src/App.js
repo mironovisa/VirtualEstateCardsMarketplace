465a493b0b1d4d1f612db5d6f18e39192e63d921
TonyOz <oz101010@gmail.com>
2023-09-05 18:04:57 +0300

    Implemented Cart Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, NavLink, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import LandingPage from './Views/LandingView/LandingPage';
 import AdminPage from './Views/AdminDashboardView/AdminPage';
 import ProfilePage from './Views/ProfileView/ProfilePage';
 import AboutPage from './Views/AboutView/AboutPage';
 import HomePage from './Views/HomeView/HomePage';
 import SigninModal from './Views/SignInModal'; 
+import { CartView } from 'Views/CartView/CartView'; 
 import { useContext } from 'react';
 import { authContext } from 'Auth/authContext';
 
 function App() {
     const { isLoggedIn, logoutUser } = useContext(authContext);
     const [modalComponent, setModalComponent] = useState(null);
+    const [cart, setCart] = useState(false);
     const userIcon = localStorage.getItem('userIcon');
     const location = useLocation();
     
     console.log('isLoggedIn:', isLoggedIn);
 
+    const handleCart = () => {
+        setCart(true);
+    }
+    
     return (
         <div className="app">
             <nav className='links-container'>
@@ -38,6 +46,7 @@ function App() {
                 ) : (
                     <>
                         <div className='right-nav'>
+                            <button className="cart-btn nav-link underline-effect" onClick={handleCart}><FontAwesomeIcon icon={faCartShopping} /></button>
                             <button className="logout-btn nav-link underline-effect" onClick={logoutUser}>Logout</button>
                             <div className="user-icon">{userIcon}</div>
                         </div>
@@ -51,6 +60,7 @@ function App() {
                         <Route path="/home" element={<HomePage />} />
                         <Route path="/profile" element={<ProfilePage />} />
                         <Route path="/about" element={<AboutPage />} />
+                        <Route path="/cart" element={<CartView />} />
                         {isLoggedIn && <Route path="/dashboard" element={<AdminPage />} />}
                        
                         <Route path="/login" element={<SigninModal  setModalComponent={setModalComponent} />} />
@@ -60,6 +70,7 @@ function App() {
                     {modalComponent && (
                         <SigninModal setModalComponent={setModalComponent} />
                     )}
+                    {cart && <CartView state={cart} onChange={setCart} />}
                 </section>
             </main>
         </div>
