de4979aba4c7ba3c9475a3075aa72a397ab58e0c
Adam Eyassu Solomon <adamyaso@gmail.com>
2023-09-12 00:48:42 +0300

    minor fixes

--- backend/utils/dailyUpload.js
+++ backend/utils/dailyUpload.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule');
 const axios = require('axios');
 
 const dailyUpload = () => {
-  schedule.scheduleJob({ second: 10, minute: 27, hour: 0 }, async () => {
+  schedule.scheduleJob({ hour: 18 }, async () => {
     for (let i = 0; i < 10; i++) {
       console.log('Generating image...');
       await axios.post('http://localhost:3001/generate-image');
