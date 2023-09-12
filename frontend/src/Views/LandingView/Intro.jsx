82966dc93ba2e35f4fa243b67853eb72dfc13833
Yonatan Abramovich <yonatanabra99@gmail.com>
2023-09-12 10:47:07 +0300

    logo + name done

--- frontend/src/Views/LandingView/Intro.jsx
+++ frontend/src/Views/LandingView/Intro.jsx
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <div className="intro-container">
         <span className='intro-title'>
-        Welcome to the Dall-e Images Marketplace: Your Destination for Stunning Images!
+        Welcome to the Art-e Gallery: Your Destination for Stunning Art Pieces!
         </span>
         <span className='intro-text'>
         Welcome to our virtual art gallery, where AI's creative prowess shines. Dive into a world of captivating AI-generated images that push the boundaries of digital artistry. Immerse yourself in this fusion of technology and imagination, as AI's brushstrokes craft a unique tapestry of visual wonder. Join us on this journey through the limitless possibilities of artificial intelligence in art. Enjoy the view!
